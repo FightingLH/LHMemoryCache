6c85de03fd293157ce205e78e3d7e2af4a91649c
lh <feeyo@feeyodeMac-mini.local>
2018-02-02 09:52:59 +0800

    no message

---
+++ LHMemoryCache/LHMemoryCache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHMemoryCache
+//
+//  Created by feeyo on 2018/2/2.
+//  Copyright © 2018年 lihuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
